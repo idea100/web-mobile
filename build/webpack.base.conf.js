25c3207a12e28ee6ef511d80dfcc5eca08ada411
huxiang <517626980@qq.com>
2017-10-31 15:24:51 +0800

    modify

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -26,7 +26,9 @@ let webpackConfig = {
     extensions: ['.js', '.vue', '.json'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src')
+      '@': resolve('src'),
+      'components': resolve('src/components'),
+      'router': resolve('src/router')
     }
   },
   module: {
