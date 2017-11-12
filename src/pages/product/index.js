5659d69d0dd3ede29908b3caa18cc049104064bf
huxiang <517626980@qq.com>
2017-11-13 02:00:16 +0800

    add jsx

--- src/pages/product/index.js
+++ src/pages/product/index.js
@@ -1,3 +1,15 @@
-import Product from './product'
+// import Product from './product'
+//
+// export default Product
 
-export default Product
+export default {
+  functional: true,
+  render () {
+    return (
+      <div>
+        <div> render function with jsx </div>
+        <div> <button onClick={() => alert('you click me!')}>click me</button> </div>
+      </div>
+    )
+  }
+}
